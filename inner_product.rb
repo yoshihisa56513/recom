446ba19e4b4bb4d710bd733f125f34653a00c893
Sho Hashimoto <hashimoto@shokai.org>
2014-07-14 17:04:36 +0900

    inner_product.rb を書き直した

--- inner_product.rb
+++ inner_product.rb
@@ -1,25 +1,16 @@
 testhash1 = {"red" => 1, "blue" => 5, "green" => 5}
 testhash2 = {"red" => 1, "blue" => 3, "green" => 6}
-$naiseki = 0
 
-def inner_product(hash1,hash2)
-	
+def inner_product(hash1, hash2)
+  naiseki = 0
 
-	hash1.each do |key1, value1|
-		hash2.each do |key2, value2| 
+  hash1.keys.each do |k|
+    next unless hash1.has_key? k and hash2.has_key? k
+    naiseki += hash1[k] * hash2[k]
+  end
 
-			if key1 == key2
-				$naiseki = $naiseki + hash1[key1] * hash2[key2]
-				
-			end
-			
-		end
-
-	end
-
-	return $naiseki
-
+  return naiseki
 end
 
 
-puts inner_product(testhash1,testhash1)
+puts inner_product(testhash1, testhash2)
