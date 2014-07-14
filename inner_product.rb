c27bc2679ea22a077a8e99e07c59aca95cae7ded
fukuda56513 <fukuda56513@gmail.com>
2014-07-14 16:56:56 +0900

    inner_product

--- inner_product.rb
+++ inner_product.rb
@@ -1,10 +1,15 @@
+testhash1 = {"red" => 1, "blue" => 5, "green" => 5}
+testhash2 = {"red" => 1, "blue" => 3, "green" => 6}
+$naiseki = 0
+
 def inner_product(hash1,hash2)
+	
 
 	hash1.each do |key1, value1|
 		hash2.each do |key2, value2| 
 
 			if key1 == key2
-				naiseki += hash1[key1] * hash2[key2]
+				$naiseki = $naiseki + hash1[key1] * hash2[key2]
 				
 			end
 			
@@ -12,6 +17,9 @@ def inner_product(hash1,hash2)
 
 	end
 
-	return naiseki
+	return $naiseki
 
 end
+
+
+puts inner_product(testhash1,testhash1)
